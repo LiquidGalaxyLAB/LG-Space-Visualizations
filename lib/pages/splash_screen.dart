53c006e76f790cad57cdab1b08e84f46152ae969
Baggi <39710037+0xbaggi@users.noreply.github.com>
2024-06-04 18:44:48 +0200

    feat: add the logos on the splash screen

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -11,7 +11,7 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
-
+  @override
   void initState() {
     super.initState();
 
@@ -23,18 +23,95 @@ class _SplashPageState extends State<SplashPage> {
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: secondaryColor,
-      body: Center(child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Spacer(),
-            const Logo(),
-            const Spacer(),
-            LinearProgressIndicator(color: backgroundColor, valueColor:  AlwaysStoppedAnimation<Color>(secondaryColor)),
-          ])),
+      body: Center(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Spacer(),
+                const Logo(),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset('assets/images/logos/liquidGalaxy.png',
+                      fit: BoxFit.contain, scale: 3),
+                  Image.asset(
+                    'assets/images/logos/GSoC.png',
+                    fit: BoxFit.contain,
+                    scale: 2.5,
+                  ),
+                  Image.asset(
+                    'assets/images/logos/anniversary.png',
+                    fit: BoxFit.contain,
+                    scale: 3.2,
+                  ),
+                  const SizedBox(width: 70),
+                ]),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset(
+                    'assets/images/logos/lgEu.png',
+                    fit: BoxFit.contain,
+                    scale: 13,
+                  ),
+                  Image.asset(
+                    'assets/images/logos/lgLab.png',
+                    fit: BoxFit.contain,
+                    scale: 3,
+                  ),
+                  Image.asset(
+                    'assets/images/logos/GDGLleida.png',
+                    fit: BoxFit.contain,
+                    scale: 1.8,
+                  ),
+                  Image.asset(
+                    'assets/images/logos/flutterLleida.png',
+                    fit: BoxFit.contain,
+                    scale: 4,
+                  ),
+                  const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/logos/labTIC.png',
+                    fit: BoxFit.contain,
+                    scale: 4,
+                  ),
+                  const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/logos/AgrobioTechLleida.png',
+                    fit: BoxFit.contain,
+                    scale: 4,
+                  ),
+                ]),
+                Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                  Image.asset(
+                    'assets/images/logos/uni.png',
+                    fit: BoxFit.contain,
+                    scale: 1.8,
+                  ),
+                  const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/logos/PERA.png',
+                    fit: BoxFit.contain,
+                    scale: 4,
+                  ),
+                  const SizedBox(width: 10),
+                  Image.asset(
+                    'assets/images/logos/flutter.png',
+                    fit: BoxFit.contain,
+                    scale: 13,
+                  ),
+                  const SizedBox(width: 20),
+                  Image.asset(
+                    'assets/images/logos/android.png',
+                    fit: BoxFit.contain,
+                    scale: 25,
+                  ),
+                ]),
+                const SizedBox(height: 2),
+                SizedBox(
+                    height: 4,
+                    child: LinearProgressIndicator(
+                        color: backgroundColor, backgroundColor: secondaryColor)),
+              ])),
     );
   }
 }
