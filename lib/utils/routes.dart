86f1904d008c5b5dc345f8f239d1a8a431d62fdd
Baggi <39710037+0xbaggi@users.noreply.github.com>
2024-06-05 17:18:08 +0200

    chore: fix routes

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -3,22 +3,29 @@ import 'package:lg_space_visualizations/pages/home_page.dart';
 import 'package:lg_space_visualizations/pages/splash_screen.dart';
 
 Route<dynamic> makeRoute(RouteSettings settings) {
+  WidgetBuilder builder;
   switch (settings.name) {
-    case '/splash':
-      return customRoute(const SplashPage());
     case '/home':
-      return customRoute(const HomePage());
+      builder = (BuildContext context) => const HomePage();
+      break;
+    case '/splash':
+      builder = (BuildContext context) => const SplashPage();
+      break;
     default:
-      return customRoute(const HomePage());
+      builder = (BuildContext context) => const HomePage();
   }
+  return AnimationPageRoute(builder: builder, settings: settings);
 }
 
-PageRouteBuilder customRoute(Widget page) {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation1, animation2) => page,
-    transitionDuration: const Duration(milliseconds: 200),
-    transitionsBuilder: (context, animation, animation2, child) {
-      return FadeTransition(opacity: animation, child: child);
-    },
-  );
+class AnimationPageRoute<T> extends MaterialPageRoute<T> {
+  AnimationPageRoute({required super.builder, super.settings});
+
+  @override
+  Duration get transitionDuration => const Duration(milliseconds: 0);
+
+  @override
+  Widget buildTransitions(BuildContext context, Animation<double> animation,
+      Animation<double> secondaryAnimation, Widget child) {
+    return child;
+  }
 }
