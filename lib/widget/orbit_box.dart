11068a9247a995a93fe61642a0532edb48e288e7
Baggi <39710037+0xbaggi@users.noreply.github.com>
2024-07-25 18:07:17 +0200

    feat: implement OrbitBox

---
+++ lib/widget/orbit_box.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:lg_space_visualizations/utils/orbit.dart';
+import 'package:lg_space_visualizations/utils/styles.dart';
+import 'package:lg_space_visualizations/widget/button.dart';
+
+/// A widget box for an [orbit].
+class OrbitBox extends StatefulWidget {
+  /// The [Orbit] instance.
+  final Orbit orbit;
+
+  const OrbitBox({super.key, required this.orbit});
+
+  @override
+  _OrbitBoxState createState() => _OrbitBoxState();
+}
+
+class _OrbitBoxState extends State<OrbitBox> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+        onTap: () {
+          setState(() {
+            setState(() {
+              Navigator.pushNamed(context, '/orbit', arguments: widget.orbit);
+            });
+          });
+        },
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(borderRadius),
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(borderRadius),
+                  child: Container(
+                    color: backgroundColor,
+                  ),
+                ),
+              ),
+              Positioned(
+                  left: spaceBetweenWidgets / 2,
+                  right: spaceBetweenWidgets / 2,
+                  bottom: spaceBetweenWidgets / 2,
+                  child: Button(
+                    icon: null,
+                    onPressed: () {},
+                    text: widget.orbit.orbitName,
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.circular(borderRadius),
+                  )),
+            ],
+          ),
+        ));
+  }
+}
